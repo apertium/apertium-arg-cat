74e4a296f333aa23ccc95f376aafa2a8e6ce24e2
Juan Pablo Martínez Cortés <jpmart@unizar.es>
2016-01-14 06:34:01 +0000

    testvoc almost clear; directional restriction for valencian forms; fix problem in words like parentesi, crisi

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,5 +1,5 @@
 echo "==Aragonese->Catalan==========================";
-bash inconsistency.sh arg-cat ../../../../languages/apertium-arg/apertium-arg.arg.dix > /tmp/arg-cat.testvoc; sh inconsistency-summary.sh /tmp/arg-cat.testvoc arg-cat 
+bash inconsistency.sh arg-cat ../../../../languages/apertium-arg/apertium-arg.arg.dix > /tmp/arg-cat.testvoc; bash inconsistency-summary.sh /tmp/arg-cat.testvoc arg-cat 
 echo ""
 echo "==Catalan->Aragonese===========================";
 bash inconsistency.sh cat-arg ../../../../languages/apertium-cat/apertium-cat.cat.dix > /tmp/cat-arg.testvoc; bash inconsistency-summary.sh /tmp/cat-arg.testvoc cat-arg
