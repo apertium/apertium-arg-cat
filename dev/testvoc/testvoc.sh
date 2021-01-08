0744f2ef85e974a8fc619bd4909b6e95e83e7b78
Juanpabl <jpmart@unizar.es>
2021-01-08 08:37:21 +0000

    fixes to clear testvoc arg-cat

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -2,4 +2,4 @@ echo "==Aragonese->Catalan==========================";
 bash inconsistency.sh arg-cat ../../../apertium-arg/apertium-arg.arg.dix > arg-cat.testvoc; bash inconsistency-summary.sh arg-cat.testvoc arg-cat 
 echo ""
 echo "==Catalan->Aragonese===========================";
-bash inconsistency.sh cat-arg ../../../apertium-cat/apertium-cat.cat.dix > cat-arg.testvoc; bash inconsistency-summary.sh cat-arg.testvoc cat-arg
+bash inconsistency.sh cat-arg ../../../apertium-cat/apertium-cat.cat.metadix > cat-arg.testvoc; bash inconsistency-summary.sh cat-arg.testvoc cat-arg
